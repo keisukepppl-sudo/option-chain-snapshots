d937cd285b04dc44d550b41899c6ce00f9818800
keisukepppl-sudo <keisukepppl@gmail.com>
2026-06-14 11:22:28 +0900

    Add trend compatibility wrapper

---
+++ scanner/trend.py
@@ -0,0 +1,5 @@
+"""Compatibility wrapper for the trend-template module."""
+
+from .trend_template import TrendTemplateResult, evaluate_trend_template
+
+__all__ = ["TrendTemplateResult", "evaluate_trend_template"]
